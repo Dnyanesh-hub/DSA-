67f47395dca513e5e1e3fc081d6187c41fa86d40
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-05-13 19:29:06 +0530

    implement higher bound

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,46 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int binarySearch(vector<int>&arr,int n,int target){
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int mid=(low+high)/2;
-        if(arr[mid]==target){
+int binarySearch(vector<int> &arr, int n, int target)
+{
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+        {
             return mid;
         }
-        else if(arr[mid]<target){
-            low=mid+1;
+        else if (arr[mid] < target)
+        {
+            low = mid + 1;
         }
-        else{
-            high=mid-1;
+        else
+        {
+            high = mid - 1;
         }
     }
     return -1;
 }
-int binarySearchrec(vector<int>&arr,int low ,int high,int target){
-    if(low>high){
+int binarySearchrec(vector<int> &arr, int low, int high, int target)
+{
+    if (low > high)
+    {
         return -1;
     }
-    int mid=(low+high)/2;
-    if(arr[mid]==target){
+    int mid = (low + high) / 2;
+    if (arr[mid] == target)
+    {
         return mid;
     }
-    else if(target>arr[mid]){
-        return binarySearchrec(arr,mid+1,high,target);
+    else if (target > arr[mid])
+    {
+        return binarySearchrec(arr, mid + 1, high, target);
     }
-    else{
-        return binarySearchrec(arr,low,mid-1,target);
+    else
+    {
+        return binarySearchrec(arr, low, mid - 1, target);
     }
 }
-int main(){
+int lowerBound(vector<int>&arr,int n,int x){
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]>=x){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+int higherBound(vector<int>&arr,int n,int x){
+    
+}
+int main()
+{
     vector<int> arr;
     int n, x;
 
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> x;
         arr.push_back(x);
     }
-    int k=binarySearchrec(arr,0,n-1,95);
-    cout<<"the number is present at:"<<k<<"th index";
+    int k = lowerBound(arr, n, 66);
+    cout << "the lower bound is present at:" << k << "th index";
 }
