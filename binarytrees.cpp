ab5ea6011945b211a600404ab3400a4073e858a7
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-05-28 11:51:52 +0530

    anticlockwise boundary traversal of binary tree

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -122,3 +122,72 @@ public:
         return ans;
     }
 };
+ class BoundaryOrder{
+    
+    bool isLeaf(TreeNode* node){
+    return (node->left == NULL && node->right == NULL);
+}
+    void leftBoundary(TreeNode*root,vector<int>&res){
+        TreeNode*cur=root->left;
+        // travels till the leaf node and go go left and if there is no left then and then only go to right 
+        while(cur){     
+            if(!isLeaf(cur)){
+                res.push_back(cur->val);
+            }
+            if(cur->left){
+                cur=cur->left;
+            }
+            else {
+                cur=cur->right;
+            }
+
+        }
+    }
+    void rightBoundary(TreeNode*root,vector<int>&res){
+        TreeNode*cur=root->right;
+        vector<int>temp;
+        while(cur){
+            if(!isLeaf(cur)){
+                temp.push_back(cur->val);
+            }
+            if(cur->right){
+                cur=cur->right;
+            }
+            else{
+                cur=cur->left;
+            }
+        }
+        for(int i=temp.size()-1;i>=0;--i){
+            res.push_back(temp[i]);
+        }
+
+    }
+    void addLeaves(TreeNode*root,vector<int>&res){
+        if(isLeaf(root)){
+            res.push_back(root->val);
+            return;
+        }
+        if(root->left){
+            addLeaves(root->left,res);
+    
+        }
+        if(root->right){
+            addLeaves(root->right,res);
+        }
+    }
+    public:
+    vector<int>printBoundary(TreeNode*root){
+        vector<int>res;
+        if(root==NULL){
+            return res;
+        }
+        if(!isLeaf(root)){
+            res.push_back(root->val);
+        }
+        leftBoundary(root,res);
+        addLeaves(root,res);
+        rightBoundary(root,res);
+        return res;
+    }
+
+ };
