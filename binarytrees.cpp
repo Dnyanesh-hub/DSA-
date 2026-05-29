51d40180776498bfc4db0175314ea960033057ec
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-05-29 14:00:00 +0530

    bottom view of the binary tree

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -312,3 +312,32 @@ vector<int> topView(TreeNode *root)
         return ans;
     }
 }
+
+// bottom view of the binary tree
+vector<int>bottomView(TreeNode*root){
+    vector<int>ans;
+    if(root==NULL){
+        return ans;
+    }
+   map<int,int>mp;
+   queue<pair<TreeNode*,int>>q;
+   q.push({root,0});
+   while(!q.empty()){
+      auto it=q.front();
+      q.pop();
+      TreeNode*node=it.first;
+      int line=it.second;
+      mp[line]=node->val;
+      if(node->left){
+        q.push({node->left,line-1});
+      }
+      if(node->right){
+        q.push({node->right,line+1});
+      }
+
+   }
+   for(auto it:mp){
+    ans.push_back(it.second);
+   }
+   return ans;
+}
