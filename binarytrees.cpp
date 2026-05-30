a01ca356bc68200339a743cb0a1b04b2fa58369d
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-05-30 12:00:00 +0530

    print nodes at k distances from given target node

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -489,4 +489,93 @@ void reorder(TreeNode*root){
         root->val=tot;
     }
 }
+// print nodes at distance k from the given target node
+void markParents(TreeNode* root,
+                 TreeNode* target,
+                 unordered_map<TreeNode*, TreeNode*>& track) 
+
+{
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode* current = q.front();
+        q.pop();
+
+        if(current->left){
+            track[current->left] = current;
+            q.push(current->left);
+        }
+
+        if(current->right){
+            track[current->right] = current;
+            q.push(current->right);
+        }
+    }   
+}
+
+vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+
+    unordered_map<TreeNode*,TreeNode*> track;
+    
+
+    markParents(root,target,track);
+
+    unordered_map<TreeNode*,bool> visited;
+    
+
+    queue<TreeNode*> q;
+    q.push(target);
+    visited[target] = true;
+
+    int level = 0;
+
+    while(!q.empty()){
+
+        int size = q.size();
+
+        if(level++ == k){
+            break;
+        }
+
+        for(int i=0;i<size;i++){
+
+            TreeNode* curr = q.front();
+            q.pop();
+
+            if(curr->left && !visited[curr->left]){
+                
+
+                q.push(curr->left);
+                visited[curr->left] = true;
+            }
+
+            if(curr->right && !visited[curr->right]){
+                q.push(curr->right);
+                visited[curr->right] = true;
+            }
+
+            if(track[curr] && !visited[track[curr]]){
+                
+
+                q.push(track[curr]);
+                
+
+                visited[track[curr]] = true;
+            
+            }
+        }
+    }
+
+    vector<int> result;
+
+    while(!q.empty()){
+        TreeNode* curr = q.front();
+        q.pop();
+        result.push_back(curr->val);
+    }
+
+    return result;
+}
+
 
