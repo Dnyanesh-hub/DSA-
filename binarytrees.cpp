dcddaeebde49392cc658df0ad174f3945d865813
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-06 13:42:52 +0530

    morris traversal inorder

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -893,3 +893,38 @@ TreeNode *deserialize(string data)
     }
     return root;
 }
+// morris inorder traversal using threaded binary tree concept morris tree gives us O(n) time complexity and O(1) space comeplexity
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    TreeNode *curr = root;
+    while (curr != NULL)
+    {
+
+        if (curr->left == NULL)
+        {
+            ans.push_back(curr->val);
+            curr = curr->right;
+        }
+        else
+        {
+            TreeNode *prev = curr->left;
+            while (prev->right && prev->right != curr)
+            {
+                prev = prev->right;
+            }
+            if (prev->right == NULL)
+            {
+                prev->right = curr;
+                curr = curr->left;
+            }
+            else
+            {
+                prev->right = NULL;
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
+    }
+    return ans;
+}
