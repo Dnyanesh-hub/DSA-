a5dc5a699c91da06c4952f17e021004e625f2def
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-10 22:54:15 +0530

    lowest common ancestor in binary search tree

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -1301,3 +1301,22 @@ TreeNode *deleteNode(TreeNode *root, int key)
     }
     return temp;
 }
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    int curr = root->val;
+    if (curr < p->val && curr < q->val)
+    {
+        return lowestCommonAncestor(root->right, p, q);
+    }
+    if (curr > p->val && curr > q->val)
+    {
+
+        return lowestCommonAncestor(root->left, p, q);
+    }
+  
+  return root;
+}
