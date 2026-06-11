0f06b9d4ec8a850c4b157f3533e0633e700f8208
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-11 23:16:54 +0530

    two sum of binary serch tree using the concept of BSTIterator

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -1355,3 +1355,74 @@ bool findTarget(TreeNode *root, int k)
     }
     return false;
 }
+// two sum for the binary tree using the concept of BSTIterator that gives us the space complexity of O(h)
+class BSTIterator
+{
+public:
+    stack<TreeNode *> st;
+    bool reverse;
+
+    BSTIterator(TreeNode *root, bool isreverse)
+    {
+        reverse = isreverse;
+        pushAll(root);
+    }
+
+    void pushAll(TreeNode *node)
+    {
+        while (node != NULL)
+        {
+            st.push(node);
+
+            if (reverse)
+                node = node->right;
+            else
+                node = node->left;
+        }
+    }
+
+    bool hasNext() { return !st.empty(); }
+
+    int next()
+    {
+        TreeNode *temp = st.top();
+        st.pop();
+
+        if (!reverse)
+            pushAll(temp->right);
+        else
+            pushAll(temp->left);
+
+        return temp->val;
+    }
+};
+class Solution
+{
+public:
+    bool findTarget(TreeNode *root, int k)
+    {
+        if (root == NULL)
+            return false;
+
+        BSTIterator l(root, false); // smallest
+        BSTIterator r(root, true);  // largest
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j)
+        {
+
+            if (i + j == k)
+                return true;
+
+            else if (i + j < k)
+                i = l.next();
+
+            else
+                j = r.next();
+        }
+
+        return false;
+    }
+};
