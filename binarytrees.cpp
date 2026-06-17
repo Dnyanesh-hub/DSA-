b795c2eb20027cdf9baa138429dc6a3af093c6f7
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-17 23:52:15 +0530

    diameter of the binary tree

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -1630,3 +1630,21 @@ int maxPathSum(TreeNode *root)
     pathsum(root, maxi);
     return maxi;
 }
+// diameter of the binary tree
+int height(TreeNode *root, int &diameter)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lh = height(root->left, diameter);
+    int rh = height(root->right, diameter);
+    diameter = max(diameter, lh + rh);
+    return 1 + max(lh, rh);
+}
+int diameterOfBinaryTree(TreeNode *root)
+{
+    int dia = 0;
+    height(root, dia);
+    return dia;
+}
