171e9037ef4965de8ccf10a9fd949e418586bd60
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-20 22:49:51 +0530

    kadanes algorithm : to find maximum subarray sum

--- arrays1placement.cpp
+++ arrays1placement.cpp
@@ -167,3 +167,21 @@ void moveZeros(vector<int> &nums, int n)
     }
     
 }
+// kadanes algorithm maximum subarray sum 
+ int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>maxi){
+                maxi=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+            
+        }
+        return maxi;
+        
+    }
