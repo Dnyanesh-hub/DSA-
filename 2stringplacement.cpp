2372848668575c2e6439d77c5b87dd34302b030a
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-23 22:19:39 +0530

    valid palindrome

--- 2stringplacement.cpp
+++ 2stringplacement.cpp
@@ -1,13 +1,38 @@
 #include <bits./stdc++.h>
 using namespace std;
-void reverseString(vector<int>&s){
-    int st=0;
-    int end=s.size()-1;
-    while (st<=end)
+// reverse the string problem
+void reverseString(vector<int> &s)
+{
+    int st = 0;
+    int end = s.size() - 1;
+    while (st <= end)
     {
-        swap(s[st],s[end]);
+        swap(s[st], s[end]);
         st++;
         end--;
     }
-    
+}
+// valid palindrome
+bool isPalindrome(string s)
+{
+    int i = 0;
+    int j = s.size() - 1;
+    while (i < j)
+    {
+        while (i < j && !isalnum(s[i]))
+        {
+            i++;
+        }
+        while (i < j && !isalnum(s[j]))
+        {
+            j--;
+        }
+        if (tolower(s[i]) != tolower(s[j]))
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 }
