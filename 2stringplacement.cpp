29a6b0b4d32ba5ea30292b9e78557cd0c4460187
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-24 22:36:02 +0530

    valid anagram

--- 2stringplacement.cpp
+++ 2stringplacement.cpp
@@ -49,3 +49,15 @@ string largestOddNumber(string num)
     }
     return "";
 }
+// valid anagram
+bool isAnagram(string s, string t)
+{
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if (s == t)
+    {
+        return true;
+    }
+    else
+        return false;
+}
