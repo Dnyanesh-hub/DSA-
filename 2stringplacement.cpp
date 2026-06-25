5298ad0cf36d8e02b0e21ba334aeaeb61ffc8eef
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-25 10:56:44 +0530

    longestCommon prefix string amogst an array of strings

--- 2stringplacement.cpp
+++ 2stringplacement.cpp
@@ -61,3 +61,24 @@ bool isAnagram(string s, string t)
     else
         return false;
 }
+// find the longest common prefix string amongst an array of strings.
+string longestCommonPrefix(vector<string> &strs)
+{
+    if (strs.size() == 0)
+    {
+        return "";
+    }
+    string prefix = strs[0];
+    for (int i = 1; i < strs.size(); i++)
+    {
+        while (strs[i].find(prefix) != 0)
+        {
+            prefix = prefix.substr(0, prefix.size() - 1);
+            if (prefix == "")
+            {
+                return "";
+            }
+        }
+    }
+    return prefix;
+}
