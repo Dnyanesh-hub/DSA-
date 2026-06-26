cd68e60fafb8ca1ae299018d1af0ea360f6cb8ea
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-06-26 23:35:49 +0530

    group anagrams

--- 2stringplacement.cpp
+++ 2stringplacement.cpp
@@ -82,3 +82,20 @@ string longestCommonPrefix(vector<string> &strs)
     }
     return prefix;
 }
+// 49. Group Anagrams
+vector<vector<string>> groupAnagrams(vector<string> &strs)
+{
+    unordered_map<string, vector<string>> mpp;
+    for (string s : strs)
+    {
+        string key = s;
+        sort(key.begin(), key.end());
+        mpp[key].push_back(s);
+    }
+    vector<vector<string>> res;
+    for (auto &it : mpp)
+    {
+        res.push_back(it.second);
+    }
+    return res;
+}
