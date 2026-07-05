d695b6e72b4b8199fa3707ab76da4001a98f7947
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-07-05 20:29:32 +0530

    search in sorted rotated array

--- 4binarysearch.cpp
+++ 4binarysearch.cpp
@@ -143,3 +143,52 @@ vector<int> searchRange(vector<int> &nums, int target)
     ans.push_back(lto);
     return ans;
 }
+// search in rotated sorted array
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        // we need to identify which half of the array will be sorted as in
+        // binary search we eliminate the search space so for that purpose
+        // we need to find the sorted half first
+        // after identifying which half is soretd then will check weather
+        // that elemet lies in that range or not
+        if (nums[low] <=
+            nums[mid])
+        { // this will telll that the left half is sorted if
+          // this condtion gets satisfy
+            if (target >= nums[low] && target <= nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (target > nums[mid] && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
