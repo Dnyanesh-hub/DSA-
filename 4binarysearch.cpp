31681c9318cb526915a7d585bc87f2ca24d20573
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-07-05 20:38:17 +0530

    find minimum in rotated sorted array

--- 4binarysearch.cpp
+++ 4binarysearch.cpp
@@ -192,3 +192,32 @@ int search(vector<int> &nums, int target)
     }
     return -1;
 }
+// minimum in rotated sorted array
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+// [4,5,6,7,0,1,2] if it was rotated 4 times.
+// [0,1,2,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+// You must write an algorithm that runs in O(log n) time.
+int findMin(vector<int> &nums)
+{
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+    int ans = INT_MAX;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[low] <= nums[mid])
+        {
+            ans = min(nums[low], ans);
+            low = mid + 1;
+        }
+        else
+        {
+            ans = min(ans, nums[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
