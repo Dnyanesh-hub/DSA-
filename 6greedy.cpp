24d2ca5655b046b640ca3cd0183348f456dd50a0
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-07-08 20:09:01 +0530

    jump game 1

---
+++ 6greedy.cpp
@@ -0,0 +1,22 @@
+#include<bits./stdc++.h>
+using namespace std;
+// jump game 
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+// Return true if you can reach the last index, or false otherwise.
+bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxIndex=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i>maxIndex)
+            {
+                return false;
+            }
+            maxIndex=max(maxIndex,nums[i]+i);
+            if(maxIndex>=n-1){
+                return true;
+            }
+        }
+        return true;
+        
+    }
