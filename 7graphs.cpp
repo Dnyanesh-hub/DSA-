5330ec5ba717724cb4bd72f08d5932c509c6d9aa
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-07-09 17:42:56 +0530

    bfs of graph

--- 7graphs.cpp
+++ 7graphs.cpp
@@ -1,5 +1,6 @@
 #include <bits./stdc++.h>
 using namespace std;
+// dfs
 void dfs(int v, vector<int> adj[],
          vector<int> &visited,
          vector<int> &result)
@@ -19,4 +20,30 @@ void dfs(int v, vector<int> adj[],
             dfs(u, adj, visited, result);
         }
     }
+}
+// bfs
+vector<int> bfs(vector<vector<int>> &adj)
+{
+    
+    int n = adj.size();
+    vector<int> bfs;
+    vector<int> vis(n, 0);
+    vis[0] = 1;
+    queue<int> q;
+    q.push(0);
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                vis[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
 }
