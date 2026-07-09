c55e950622b648a223d7711a15ba6a407ef67437
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-07-09 17:45:08 +0530

    number of islands

--- 7graphs.cpp
+++ 7graphs.cpp
@@ -47,3 +47,45 @@ vector<int> bfs(vector<vector<int>> &adj)
     }
     return bfs;
 }
+// number of islands
+void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&grid,int delrow[],int delcol[]){
+    vis[row][col]=1;
+    int n=grid.size();
+    int m=grid[0].size();
+    queue<pair<int,int>>q;
+    q.push({row,col});// pushing starting node into queue
+    while(!q.empty()){
+        int r=q.front().first;
+        int c=q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nrow=r+delrow[i];
+            int ncol=c+delcol[i];
+            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0){
+                q.push({nrow,ncol});
+                vis[nrow][ncol]=1;
+            }
+
+        }
+    }
+  }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int cnt=0;
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(!vis[row][col] && grid[row][col]=='1')
+                {   
+                    cnt++;
+                    bfs(row,col,vis,grid,delrow,delcol);
+                }
+
+            }
+        }
+        return cnt;
+        
+    }
